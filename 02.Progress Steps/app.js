66be993f1635e9fab1fa6be8e84b3126e8428cac
latifii <latifii68@gmail.com>
2021-10-27 11:21:31 +0330

    Progress Steps

---
+++ 02.Progress Steps/app.js
@@ -0,0 +1,38 @@
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const progress = document.querySelector(".progress");
+const circles = document.querySelectorAll(".circle");
+let counter = 1;
+
+next.addEventListener("click", () => {
+  counter++;
+  update();
+});
+
+prev.addEventListener("click", () => {
+  counter--;
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < counter) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  if (counter === 1) {
+    prev.disabled = true;
+  } else if (counter === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+
+  const actives = document.querySelectorAll('.active');
+  progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+}
